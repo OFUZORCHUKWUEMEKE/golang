4141801020df5f3ebdacccf41ca7c4e80f60aa1d
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-07-23 00:53:56 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	const maxSpeed = 30
+	const MinPurchasePrice = 40
+
+	retrieve()
+
+	addNumbers(2, 2)
+}
+
+func retrieve() int {
+	const age = 22
+	return age
+}
+
+func addNumbers(a, b int) int {
+	return a + b
+}
