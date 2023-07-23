976a22aa9d7f01949537b2dec66031c0472ef391
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-07-23 02:16:50 +0100

    added

--- variables/main.go
+++ variables/main.go
@@ -4,5 +4,16 @@ import "fmt"
 
 func main() {
 	var myName = "Ofuzor Chukwuemeke"
+
+	username := "admin"
 	fmt.Println("My name is", myName)
+
+	fmt.Println(username)
+
+	// type name string
+
+	name := "Ofuzor Chukwuemeke"
+	part1, part2 := 1, 2
+
+	fmt.Println(part1, part2, name)
 }
