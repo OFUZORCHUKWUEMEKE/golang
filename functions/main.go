53b5698d7df3fb4acc56ab420936779a56a93983
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-07-23 16:31:27 +0100

    functions

---
+++ functions/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	two()
+	fmt.Println("Another One")
+}
+
+func two() (int, int) {
+	return 5, 5
+}
+
+func addthree(a, b, c int) int {
+	return a + b + c
+}
