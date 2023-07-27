34e03988049d5825bd65af9e61cf28120a48fb6f
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-07-27 17:36:31 +0100

    added

--- loop/main.go
+++ loop/main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Fuck Off MotherFucker")
+	sum := 0
+	fmt.Println(sum)
+	for i := 1; i <= 10; i++ {
+		sum += 1
+		fmt.Println("sum is", sum)
+	}
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Decrement. Sum is", sum)
+	}
+
+	ForLoop()
+}
+
+func ForLoop() {
+	sum := 0
+	for i := sum; i < 50; i++ {
+		divisibleBy3 := i%3 == 0
+		divisibleBy5 := i%5 == 0
+
+		if divisibleBy3 && divisibleBy5 {
+			fmt.Println("FizzBuzz")
+		} else if divisibleBy3 {
+			fmt.Println("fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
