47042a95a5380a6a45503e88d95959174ff1ef38
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-08-01 11:14:53 +0100

    adedd

---
+++ AnonExample/main.go
@@ -0,0 +1,37 @@
+package main
+
+const (
+	Form   = "fit"
+	INJURY = "injured"
+)
+
+type Player struct {
+	number   int
+	position string
+	health   string
+	starter  bool
+	star     bool
+}
+
+type GoalKeeper struct {
+	number int
+	health string
+}
+
+type Coach struct {
+	name string
+}
+
+type Squad struct {
+	players    [10]Player
+	coach      Coach
+	goalKeeper GoalKeeper
+}
+
+func (play *Squad) starter(Player) {
+
+}
+
+func main() {
+
+}
